964dbcccd0c1265f0f60800c714c9dd0a1273496
ANDREEA-LAVINIA TRÎMBIȚAȘ <andreea.trimbitas@stud.ubbcluj.ro>
2021-11-28 15:42:01 +0200

    Add project files.

---
+++ Data/Trimbitas_Andreea_Lavinia_Lab8Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Trimbitas_Andreea_Lavinia_Lab8.Models;
+
+namespace Trimbitas_Andreea_Lavinia_Lab8.Data
+{
+    public class Trimbitas_Andreea_Lavinia_Lab8Context : DbContext
+    {
+        public Trimbitas_Andreea_Lavinia_Lab8Context (DbContextOptions<Trimbitas_Andreea_Lavinia_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Trimbitas_Andreea_Lavinia_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Trimbitas_Andreea_Lavinia_Lab8.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Pages/Books/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Trimbitas_Andreea_Lavinia_Lab8.Data;
+using Trimbitas_Andreea_Lavinia_Lab8.Models;
+
+namespace Trimbitas_Andreea_Lavinia_Lab8.Pages.Books
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Trimbitas_Andreea_Lavinia_Lab8.Data.Trimbitas_Andreea_Lavinia_Lab8Context _context;
+
+        public CreateModel(Trimbitas_Andreea_Lavinia_Lab8.Data.Trimbitas_Andreea_Lavinia_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+            ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID", "PublisherName");
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Book.Add(Book);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
